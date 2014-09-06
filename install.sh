3e06ed92052f96f1279ecd49049b0c46d791ee8b
Hurricane Hamilton <hhamilto@mtu.edu>
2014-09-06 19:18:37 -0400

    Work on crontab install script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-echo "imma installer"
+echo "Adding bill-forwarder to crontab"
+
+echo "0 */1 * * *  node $(pwd)/main" | crontab -
 
 #installs main to crontab (or it will)
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
-echo im uninstallin
-
-#uninstalls crontab
-
+echo Removing the bill-forwarder from the cron tab
+crontab -l | sed "/node $(pwd | sed 's/\//\\\//g')\/main/d" | crontab -
