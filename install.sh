3bc24eeb62cd00a67866febda09122e982565928
Hurricane Hamilton <hhamilto@mtu.edu>
2014-09-07 17:26:34 -0400

    Working, semi robust cron install scripts

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
 #! /bin/bash
+CDPATH=
+OLDDIR=`pwd`
+#The following take from here: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd $DIR
+./uninstall.sh
 
 echo "Adding bill-forwarder to crontab"
 
-echo "0 */1 * * *  node $(pwd)/main" | crontab -
-
-#installs main to crontab (or it will)
+#installs main to crontab 
+(crontab -l; echo "0 */1 * * *  node $DIR/main") | crontab -
+cd $OLDDIR
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,15 @@
 #! /bin/bash
+CDPATH=
+OLDDIR=`pwd`
+#The following take from here: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-echo Removing the bill-forwarder from the cron tab
-crontab -l | sed "/node $(pwd | sed 's/\//\\\//g')\/main/d" | crontab -
+echo Removing the bill-forwarder from the cron tab, if it exists
+crontab -l | sed "/node $(echo $DIR| sed 's/\//\\\//g')\/main/d" | crontab -
+cd $OLDDIR
